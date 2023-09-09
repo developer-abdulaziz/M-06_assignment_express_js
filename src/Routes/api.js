3d45de69b1f6f5f06ddc83a39e2c6a788f9f6ae3
Md Abdul Aziz <abdulazizwebdeveloper1996@gmail.com>
2023-09-09 18:22:31 +0600

    import all controllers

---
+++ src/Routes/api.js
@@ -0,0 +1,29 @@
+// Call Express js
+const express = require('express');
+
+// Make Router Object From Express.js
+const router = express.Router();
+
+
+// Import all controllers
+const blogController = require('../Controllers/blogController');
+
+const blogDetailsController = require('../Controllers/blogDetailsController');
+
+const commentController = require('../Controllers/commentController');
+
+const messageController = require('../Controllers/messageController');
+
+const portfolioController = require('../Controllers/portfolioController');
+
+const profitController = require('../Controllers/profitController');
+
+const projectController = require('../Controllers/projectController');
+
+const serviceController = require('../Controllers/serviceController');
+
+const titleController = require('../Controllers/titleController');
+
+
+// Route all requests to the appropriate controller
+router.get()
